b593384463e979bf432e4daf67350134b9c665c1
abdulrehman-5 <84318672+abdulrehman-5@users.noreply.github.com>
2021-05-19 12:09:22 +0500

    Add files via upload

---
+++ AR_test_app.py
@@ -0,0 +1,35 @@
+import streamlit as st
+from PIL import Image
+
+st.title("Lahore Garrison University")
+st.text("Lahore Garrison University is located in the metropolitan city of Lahore")
+st.header("Digital Forensics Research and Service Centre")
+st.info("This is the first ever Private Digital Forensics and Cyber Security Research centre")
+
+st.subheader("4th Semester")
+#a = 5
+
+#Addition application
+#st.info("Please enter the number between 0 and 9")
+a = st.number_input("Please enter the number between 0 and 9")
+b = st.number_input("Please enter another number between 0 and 9")
+c = a + b
+
+
+#a = st.text_input("Please enter the number between 0 and 9")
+#b = st.text_input("Please enter another number between 0 and 9")
+#c = a + b
+st.text("The result after addition is")
+st.write(c)
+
+image = Image.open('Type_Error.png')
+st.image(image)
+
+
+
+
+
+
+
+
+
---
+++ README.md
@@ -0,0 +1,8 @@
+# web app deployed on Heroku
+
+The deployed web app is live at https://taseer-test-app.herokuapp.com/
+
+
+
+The web app was built in Python using the following libraries:
+* streamlit
